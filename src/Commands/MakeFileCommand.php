0447e98f776692005c19b3569f3bf1e13adc1aea
CristianHG2 <cherreragiraldo@gmail.com>
2022-04-20 02:07:05 +0000

    prepare release

---
+++ src/Commands/MakeFileCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace LifeSpikes\PhpBeam\Commands;
+
+use Illuminate\Console\Command;
+
+/**
+ * T
+ */
+class MakeFileCommand extends Command
+{
+    protected $signature = 'command:name';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        //
+    }
+}
